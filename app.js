8d2dae0e0e1095e61ddecfdb71b51692e4b3b68b
VagAnyf <130105542+VagAnyf@users.noreply.github.com>
2023-06-19 00:44:39 +0300

    Update app.js

--- app.js
+++ app.js
@@ -21,10 +21,10 @@ const MongoStore = require('connect-mongo')
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
-const dbUrl = 'mongodb://localhost:27017/yelp-camp'
+const dbUrl = process.env.DB_URL
 // const dbUrl = process.env.DB_URL
 // 'mongodb://localhost:27017/yelp-camp',
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
