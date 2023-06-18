1c2626c8d095c339ff0801ee0ae3d0fdf241c98b
Vagelis <vagelis.anyfantakis@gmail.com>
2023-06-19 01:57:42 +0300

    new update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV !== "production") {
     require('dotenv').config();
 }
-
+//hello there
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
@@ -21,10 +21,10 @@ const MongoStore = require('connect-mongo')
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
-const dbUrl = 'mongodb://localhost:27017/yelp-camp'
+const dbUrl = process.env.DB_URL
 // const dbUrl = process.env.DB_URL
 // 'mongodb://localhost:27017/yelp-camp',
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
